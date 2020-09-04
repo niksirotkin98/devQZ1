0343686ebb742e127464866fa7204828e1e2f5fc
niksirotkin98 <57777796+niksirotkin98@users.noreply.github.com>
2020-09-05 01:39:29 +0300

    Add files via upload

--- src/script/script.js
+++ src/script/script.js
@@ -1,6 +1,6 @@
 'use strict'
 
-const maxMoveLenght = 200;
+let maxMoveLenght = 0.1;
 
 function Vector(x,y){
     return {
@@ -21,6 +21,10 @@ function Vector(x,y){
         set(x,y=this.y){
             this.x = x;
             this.y = y;
+        },
+        setV(v){
+            this.x = v.x;
+            this.y = v.y;
         }
     }
 }
@@ -40,16 +44,16 @@ window.addEventListener("mousemove",({clientX:x, clientY:y})=>{
     
     v[0] = mouse.diff(v[0]);
     v[0].len = v[0].lenght();
-    v[0] = v[0].mul(1/v[0].len).mul(maxMoveLenght*((v[0].len/screenMaxLenght)**2));
+    v[0].setV(v[0].mul(1/v[0].len).mul(v[0].len*maxMoveLenght));
     v[1] = mouse.diff(v[1]);
     v[1].len = v[1].lenght();
-    v[1] = v[1].mul(1/v[1].len).mul(maxMoveLenght*((v[1].len/screenMaxLenght)**2));
+    v[1].setV(v[1].mul(1/v[1].len).mul(v[1].len*maxMoveLenght));
     //v[0] = v[0].mul(v[0].lenght()/screenMaxLenght)
-    moveables[0].style.setProperty("--x", v[0].x+"px");
-    moveables[0].style.setProperty("--y", v[0].y+"px");
-    moveables[1].style.setProperty("--x", v[1].x+"px");
-    moveables[1].style.setProperty("--y", v[1].y+"px");
-    console.log(screenMaxLenght, v[0], v[1])
+    moveables[0].style.setProperty("--x", Math.floor(v[0].x)+"px");
+    moveables[0].style.setProperty("--y", Math.floor(v[0].y)+"px");
+    moveables[1].style.setProperty("--x", Math.floor(v[1].x)+"px");
+    moveables[1].style.setProperty("--y", Math.floor(v[1].y)+"px");
+    console.log(screenMaxLenght, v[0].len, v[1].len)
 })
 
 document.fonts.ready.then(()=>{
